03e3ee486f03d489afcee1b60ac08c531b4f3152
fermin234 <fermin234@hotmail.com>
2022-08-07 15:34:55 -0300

    Se elimino persona

--- src/data.js
+++ src/data.js
@@ -26,15 +26,6 @@ let data = [
     empresa: 'Spotify',
     testimonio: '"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
 },
-{
-    key: 3,
-    nombre: 'Brisa Correa',
-    pais: 'Argentina',
-    img: 'brisa',
-    cargo: 'Moza',
-    empresa: 'Bar Gea',
-    testimonio: '"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'
-},
 ]
 
 export default data;
